c855fcc5a3fc7449c2242846a6163d790169b724
vb004 <bellosvasilios@gmail.com>
2024-10-19 14:31:08 +0300

    lab02

--- lab02.s
+++ lab02.s
@@ -1,7 +1,7 @@
 
 .data
 
-array: .word 1, 0, 1, 12, 0, 1, 4
+array: .word 1, 0, 1, 12, 0, 1, 1
 
 .text
 
@@ -12,15 +12,14 @@ prog:
 #-----------------------------
 # Write your code here!
     add s0, zero, zero
-    add s1, zero, zero
+    add t2, zero, zero
 loop:
-    beq a1 ,s1,done
+    beq a1 ,t2,done
+    addi t2 , t2 , 1
     lw t1 , 0(a0)
-    addi s1 , s1 , 1
     beq a2 ,t1,found
     addi a0,a0,4
     j loop
-    
 found:
     add s0 ,zero ,a0
     addi a0,a0,4
