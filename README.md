72d4b591da9acd1cff1dd266dc7195081aab0b95
vb004 <bellosvasilios@gmail.com>
2024-10-19 14:18:07 +0300

    lab02

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## UoI-CSE-MYY505 lab02
 
-## First-Name Last-Name
+## vasilis Bellos
 
 **TODO - Update your name in this readme. Leave 1 space after the ## to preserve the text formating (markdown)**
 
--- lab02.s
+++ lab02.s
@@ -11,6 +11,21 @@ array: .word 1, 0, 1, 12, 0, 1, 4
 prog:
 #-----------------------------
 # Write your code here!
+    add s0, zero, zero
+    add s1, zero, zero
+loop:
+    beq a1 ,s1,done
+    lw t1 , 0(a0)
+    addi s1 , s1 , 1
+    beq a2 ,t1,found
+    addi a0,a0,4
+    j loop
+    
+found:
+    add s0 ,zero ,a0
+    addi a0,a0,4
+    j loop  
+            
 # Do not remove the prog label or write code above it!
 #-----------------------------
 done:
